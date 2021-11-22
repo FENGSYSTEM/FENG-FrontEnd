b06d673957aafe0f154a127202f85dc9923818ac
Nguyễn Văn Phú Nhàn <pnnguyen9999@outlook.com.vn>
2021-11-23 03:49:52 +0700

    fix cart input

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Input, message } from "antd";
 import { updateCart } from "@redux/slices/counter";
@@ -9,8 +9,85 @@ import { DeleteOutlined } from "@ant-design/icons";
 
 interface Props {}
 
-export default function Index({}: Props): ReactElement {
+const CartItem = ({ obj, index }: { obj: any; index: number }) => {
   const dispatch = useDispatch();
+  const reduxCart = useSelector((state) => state.counter.cart);
+
+  const [itemAmountValue, setAmountValue] = useState<number>(obj.amount);
+  // const [reduxCartClone, setReduxCartClone] = useState<[]>(reduxCart);
+  let reduxCartClone = [...reduxCart] as any;
+  // console.log(reduxCartClone);
+  // useEffect(() => {
+  //   console.log("abc");
+  // }, [dispatch]);
+
+  return (
+    <div className="row cart-item">
+      <div className="col-3 cart-item-row d-flex align-items-center justify-content-center">
+        {obj.id}
+      </div>
+      <div className="col-3 cart-item-row d-flex align-items-center justify-content-center">
+        {obj.name} - {obj.size}
+      </div>
+      <div className="col-3 cart-item-row d-flex align-items-center justify-content-center">
+        <div className="d-flex align-items-center justify-content-center">
+          <Input
+            value={itemAmountValue}
+            type="number"
+            className="text-center"
+            key={`recipient_${index}`}
+            onChange={(e) => {
+              console.log(e);
+              if (parseInt(e.target.value) < 1) {
+                message.info("Minimum amount value reached !");
+              } else {
+                setAmountValue(parseInt(e.target.value));
+                const currentItemIndex = reduxCartClone.findIndex(
+                  (e: any) =>
+                    e.id === obj.id &&
+                    e.size === obj.size &&
+                    e.color === obj.color
+                );
+                let currentItem = reduxCartClone[currentItemIndex];
+                if (currentItemIndex !== -1) {
+                  reduxCartClone[currentItemIndex] = {
+                    ...currentItem,
+                    amount: e.target.value,
+                  };
+                  localStorage.setItem("cart", JSON.stringify(reduxCartClone));
+                  dispatch(updateCart(reduxCartClone));
+
+                  // message.info("Cart updated !");
+                }
+              }
+            }}
+          />
+          <div
+            className="font-11 px-2 cursor-pointer"
+            onClick={() => {
+              const currentItemIndex = reduxCartClone.findIndex(
+                (e: any) =>
+                  e.id === obj.id &&
+                  e.size === obj.size &&
+                  e.color === obj.color
+              );
+              reduxCartClone.splice(currentItemIndex, 1);
+              localStorage.setItem("cart", JSON.stringify(reduxCartClone));
+              dispatch(updateCart(reduxCartClone));
+            }}
+          >
+            <DeleteOutlined />
+          </div>
+        </div>
+      </div>
+      <div className="col-3 cart-item-row d-flex align-items-center justify-content-center">
+        ${obj.price}
+      </div>
+    </div>
+  );
+};
+
+export default function Index({}: Props): ReactElement {
   const reduxCart = useSelector((state) => state.counter.cart);
   const totalItemsInCart = useSelector(
     (state) => state.counter.totalItemsInCart
@@ -18,79 +95,7 @@ export default function Index({}: Props): ReactElement {
   const totalPriceInCart = useSelector(
     (state) => state.counter.totalPriceInCart
   );
-  const CartItem = ({ obj, index }: { obj: any; index: number }) => {
-    const [itemAmountValue, setAmountValue] = useState<number>(obj.amount);
-    // const [reduxCartClone, setReduxCartClone] = useState<[]>(reduxCart);
-    let reduxCartClone = [...reduxCart] as any;
 
-    // console.log(reduxCartClone);
-    return (
-      <div className="row cart-item">
-        <div className="col-3 cart-item-row d-flex align-items-center justify-content-center">
-          {obj.id}
-        </div>
-        <div className="col-3 cart-item-row d-flex align-items-center justify-content-center">
-          {obj.name} - {obj.size}
-        </div>
-        <div className="col-3 cart-item-row d-flex align-items-center justify-content-center">
-          <div className="d-flex align-items-center justify-content-center">
-            <Input
-              value={itemAmountValue}
-              type="number"
-              className="text-center"
-              key={`recipient_${index}`}
-              onChange={(e) => {
-                if (parseInt(e.target.value) < 1) {
-                  message.info("Minimum amount value reached !");
-                } else {
-                  setAmountValue(parseInt(e.target.value));
-                  const currentItemIndex = reduxCartClone.findIndex(
-                    (e: any) =>
-                      e.id === obj.id &&
-                      e.size === obj.size &&
-                      e.color === obj.color
-                  );
-                  let currentItem = reduxCartClone[currentItemIndex];
-                  if (currentItemIndex !== -1) {
-                    reduxCartClone[currentItemIndex] = {
-                      ...currentItem,
-                      amount: e.target.value,
-                    };
-                    localStorage.setItem(
-                      "cart",
-                      JSON.stringify(reduxCartClone)
-                    );
-                    dispatch(updateCart(reduxCartClone));
-
-                    // message.info("Cart updated !");
-                  }
-                }
-              }}
-            />
-            <div
-              className="font-11 px-2 cursor-pointer"
-              onClick={() => {
-                const currentItemIndex = reduxCartClone.findIndex(
-                  (e: any) =>
-                    e.id === obj.id &&
-                    e.size === obj.size &&
-                    e.color === obj.color
-                );
-                reduxCartClone.splice(currentItemIndex, 1);
-                localStorage.setItem("cart", JSON.stringify(reduxCartClone));
-                dispatch(updateCart(reduxCartClone));
-              }}
-            >
-              <DeleteOutlined />
-            </div>
-          </div>
-        </div>
-        <div className="col-3 cart-item-row d-flex align-items-center justify-content-center">
-          ${obj.price}
-        </div>
-      </div>
-    );
-  };
   return (
     <div className="w-100">
       <div className="col-12">
