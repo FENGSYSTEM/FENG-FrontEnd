d3f719679145bdec092a6691209d163cf9e66c6b
Nguyễn Văn Phú Nhàn <pnnguyen9999@outlook.com.vn>
2021-12-08 11:26:02 +0700

    ssr ngprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,9 +9,21 @@ import store from "@redux/store";
 import MasterLayout from "@components/MasterLayout";
 import { animated, Transition } from "react-spring";
 import Head from "next/head";
+import NProgress from "nprogress";
+import Router from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   const router = useRouter();
+  NProgress.configure({
+    minimum: 0.3,
+    easing: "ease",
+    speed: 500,
+    showSpinner: false,
+  });
+
+  Router.events.on("routeChangeStart", () => NProgress.start());
+  Router.events.on("routeChangeComplete", () => NProgress.done());
+  Router.events.on("routeChangeError", () => NProgress.done());
   const items = [
     {
       id: router.route,
